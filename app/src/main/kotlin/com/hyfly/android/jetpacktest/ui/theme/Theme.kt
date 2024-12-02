16bb9187344e6606bad0cde011fac18e13d115be
hyfly233 <hyfly233@outlook.com>
2024-12-02 21:56:58 +0800

    Update Theme.kt

--- app/src/main/kotlin/com/hyfly/android/jetpacktest/ui/theme/Theme.kt
+++ app/src/main/kotlin/com/hyfly/android/jetpacktest/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.hyfly.android.jetpacktest.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
